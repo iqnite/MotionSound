6e7db1cf781b034dceb1052eae90f5315ead77d4
iqnite <iqnitetechnologies@gmail.com>
2026-05-14 14:17:49 +0200

    Update texts

--- app/src/main/java/com/example/motionsound/MainActivity.kt
+++ app/src/main/java/com/example/motionsound/MainActivity.kt
@@ -189,11 +189,11 @@ fun MotionSoundScreen(
                 Text(
                     text = when (page) {
                         0 -> "Won't play any sound."
-                        1 -> "Move the phone around to play sound."
-                        2 -> "Quickly move the phone up and down to play sound."
-                        3 -> "Throw the phone to play sound."
-                        4 -> "Move the phone faster for higher pitch"
-                        else -> "Page $page"
+                        1 -> "Move the phone around to play sound!"
+                        2 -> "Quickly move the phone up and down to play sound and flash the torch!"
+                        3 -> "Throw the phone to play sound!"
+                        4 -> "Move the phone faster for higher pitch!"
+                        else -> "I forgot to add text to this page."
                     },
                     modifier = Modifier
                         .fillMaxWidth()
